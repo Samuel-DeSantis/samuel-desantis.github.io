01b2a7ae8a273565a76b1f2215c03eeeaa305313
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2023-08-27 16:17:27 -0600

    new portfolio

---
+++ index.js
@@ -0,0 +1,28 @@
+const climbimage = document.getElementById("climbs-img")
+const msgimage = document.getElementById("msg-img")
+
+function FirstClimb () {
+  // console.log(image)
+  climbimage.src = "./public/images/climbs/Climbs1.png"
+}
+
+function SecondClimb () {
+  climbimage.src = "./public/images//climbs/Climbs2.png"
+}
+
+function ThirdClimb () {
+  climbimage.src = "./public/images/climbs/Climbs3.png"
+}
+
+function FirstMsgBoard () {
+  // console.log(image)
+  msgimage.src = "./public/images/msg-board/MsgBoard1.png"
+}
+
+function SecondMsgBoard () {
+  msgimage.src = "./public/images//msg-board/MsgBoard2.png"
+}
+
+function ThirdMsgBoard () {
+  msgimage.src = "./public/images/msg-board/MsgBoard3.png"
+}
